35c710f2c1ac4e608e84aa767c47dc24f6178f90
Gabriel Peralta <prates_peralta@hotmail.com>
2022-05-04 08:48:54 -0300

    install express.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
 const express = require('express');
-const { default: appointmentsRouter } = require('./routers/appointmentsRouter');
+// const appointmentsRouter = require('./routers/appointmentsRouter');
 
 const app = express();
 
-app.use('/appointments', appointmentsRouter);
+app.use('/', (req, res) => {
+  res.send('hello world');
+});
+// app.use('/appointments', appointmentsRouter);
 
 app.listen(3001, () => {
   console.log('ouvindo na porta 3000');
