c9f8104f01b1e6759bd96a0ae4649c9af0232562
Gabriel Peralta <prates_peralta@hotmail.com>
2022-05-05 15:24:40 -0300

    Create env variables

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const appointmentsRouter = require('./routers/appointmentsRouter');
 const userRouter = require('./routers/usersRouter');
 
-require('dotenv').config();
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +12,4 @@ app.use(express.json());
 app.use('/appointments', appointmentsRouter);
 app.use('/users', userRouter);
 
-app.listen(3001, () => {
-  console.log('ouvindo na porta 3001');
-});
+app.listen(PORT);
